4afb61ab40c5ca7cfe3e66f134e1d0f9888a3adf
Ondřej Motlíček <barerovom@gmail.com>
2019-11-09 22:49:40 +0100

    Přidejte soubory projektu.

---
+++ CarcassonneDiscovery.UserClient/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace CarcassonneDiscovery.UserClient
+{
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
